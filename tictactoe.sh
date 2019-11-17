fa8ca74fd73a2c6b43812d7e63439aa1b6bda46b
zafaraabid <zafaraabid3295@gmail.com>
2019-11-17 12:17:16 +0530

    adding functionality to assign player either X or O symbol

--- tictactoe.sh
+++ tictactoe.sh
@@ -5,6 +5,8 @@ echo "----welcome----"
 #declaring constants
 declare NO_OF_ROW_COLUMNS=3;
 declare BOARD_SIZE=$(($NO_OF_ROW_COLUMNS * $NO_OF_ROW_COLUMNS))
+declare PLAYER
+declare COMPUTER
 
 #declaring varibles
 
@@ -12,12 +14,28 @@ declare BOARD_SIZE=$(($NO_OF_ROW_COLUMNS * $NO_OF_ROW_COLUMNS))
 #declaring directories
 declare -a boardOfTicTacToe
 
+function letterAssignment()
+{
+	random=$((RANDOM%2))
+	if [ $random == 1 ]
+	then
+			PLAYER="X"
+			COMPUTER="O"
+			echo "PLAYER had assigned "$PLAYER
+	else
+         PLAYER="O"
+         COMPUTER="X"
+         echo "PLAYER had assigned "$PLAYER
+	fi
+}
+
 function resetTheBoard()
 {
- for (( places=1; places <=$BOARD_SIZE; places++ ))
- do
-   boardOfTicTacToe[$places]="-";
- done
+	letterAssignment
+ 	for (( places=1; places <=$BOARD_SIZE; places++ ))
+ 	do
+ 		  boardOfTicTacToe[$places]="-";
+ 	done
 }
 
 resetTheBoard
