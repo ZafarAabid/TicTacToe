039a754fc3b55d0efe5ceb56ab1a4f910dafc4ef
zafaraabid <zafaraabid3295@gmail.com>
2019-11-17 12:04:42 +0530

    adding functionality to reset the board

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,3 +1,24 @@
 #!/bin/bash -x
 
 echo "----welcome----"
+
+#declaring constants
+declare NO_OF_ROW_COLUMNS=3;
+declare BOARD_SIZE=$(($NO_OF_ROW_COLUMNS * $NO_OF_ROW_COLUMNS))
+
+#declaring varibles
+
+
+#declaring directories
+declare -a boardOfTicTacToe
+
+function resetTheBoard()
+{
+ for (( places=1; places <=$BOARD_SIZE; places++ ))
+ do
+   boardOfTicTacToe[$places]="-";
+ done
+}
+
+resetTheBoard
+echo ${boardOfTicTacToe[@]}
